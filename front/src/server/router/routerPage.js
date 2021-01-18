85e95c2779c8b534c3c1d7e595eb1d7b600ff8a3
Antoine Richard <56544866+arichard-info@users.noreply.github.com>
2021-01-19 00:56:15 +0100

    feat(server): Load components on server start (#2)
    Co-authored-by: Antoine Richard <antoiner.info@gmail.com>

--- front/src/server/router/routerPage.js
+++ front/src/server/router/routerPage.js
@@ -2,12 +2,22 @@ import express from "express";
 import routes from "@/router";
 import middlewareData from "@/server/middlewares/data";
 import middlewareRender from "@/server/middlewares/render";
+import { preloadComponents } from "@/server/utils/preloadComponents";
 
 const router = express.Router();
 
-Object.entries(routes).map(([type, route]) => {
-  const config = { ...route, type };
-  router.get(route.path, middlewareData(config), middlewareRender(config));
+// Pre-load components server-side on init
+// No need to load them on render
+preloadComponents().then((registry) => {
+  Object.entries(routes).map(([type, route]) => {
+    const config = {
+      type,
+      blocks: registry.blocks,
+      ...route,
+      ...registry.routes[type],
+    };
+    router.get(route.path, middlewareData(config), middlewareRender(config));
+  });
 });
 
 export default router;
